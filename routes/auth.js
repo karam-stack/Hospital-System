82778c11027ff54bbc6baa3ab9d1200415773fef
MohammadALabeed <mbyd313@gmail.com>
2026-06-03 14:29:57 -0900

    تنظيف وحفظ الملفات الحالية

--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,15 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const { login, logout } = require('../controllers/authController');
-const authenticate = require('../middleware/authMiddleware');
-const validate = require('../middleware/validate');
-const loginSchema = require('../validations/loginValidation');
+const { login, logout } = require("../controllers/authController");
+const authenticate = require("../middleware/authMiddleware");
+const validate = require("../middleware/validate");
+const loginSchema = require("../validations/authValidation");
 
 // LOGIN
-router.post('/login', validate(loginSchema), login);
+router.post("/login", validate(loginSchema), login);
 
 // LOGOUT (protected)
-router.post('/logout', authenticate, logout);
+router.post("/logout", authenticate, logout);
 
 module.exports = router;
